a098592253cac6af39eff19c570c32f1db2fa309
HereIsSee <biramantvydas@gmail.com>
2024-08-13 21:18:28 +0300

    Add #get_moves in Knight class

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,6 +1,29 @@
-class Knight
+require_relative 'piece'
+
+class Knight < Piece
+  
+  KNIGHT_MOVES = [
+  [2, 1], [2, -1], [-2, 1], [-2, -1],
+  [1, 2], [1, -2], [-1, 2], [-1, -2]
+  ]
+  
   def initialize(color)
     @color = color
+  end
+
+  def get_moves(current_position, board)
+    moves = []
+
+    KNIGHT_MOVES.each do |move|
+      new_x = current_position[0] + move[0]
+      new_y = current_position[1] + move[1]
+      
+      new_position = [new_x, new_y]
+      
+      moves << new_position if is_within_board(new_x, new_y) && !friendly_piece?
+    end
+
+    moves
   end
 
   def get_color
@@ -13,4 +36,6 @@ class Knight
     
     'error'
   end
+
+
 end
