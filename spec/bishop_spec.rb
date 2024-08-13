6c9b2da57a93ecb2740b443c56af93e782c70b66
HereIsSee <biramantvydas@gmail.com>
2024-08-13 21:05:14 +0300

    Add tests for #get_moves in Bishop class

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -18,9 +18,36 @@ describe Bishop do
         ["", "", "", "", "", "", "", ""], 
         ["", "", "", "", "", "", "", ""]]
         )
-        p moves
+        expect(moves.count).to eq(7)
       end
-    
+
+      it 'returns 0 moves when white bishop is placed in [0, 0] position and in position [1, 1] is a white pawn' do
+        moves = bishop_moves.get_moves([0, 0], 
+        [["wb", "", "", "", "", "", "", ""],
+        ["", "wp", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""],
+        ["", "", "", "", "", "", "", ""]]
+        )
+        expect(moves.count).to eq(0)
+      end
+      
+      it 'returns 9 moves when placed in [1, 1] position' do
+        moves = bishop_moves.get_moves([1, 1], 
+        [["", "", "", "", "", "", "", ""], 
+        ["", "wb", "", "", "", "", "", ""], 
+        ["", "", "", "", "", "", "", ""], 
+        ["", "", "", "", "", "", "", ""], 
+        ["", "", "", "", "", "", "", ""], 
+        ["", "", "", "", "", "", "", ""], 
+        ["", "", "", "", "", "", "", ""], 
+        ["", "", "", "", "", "", "", ""]]
+        )
+        expect(moves.count).to eq(9)
+      end
     end
   end
 
