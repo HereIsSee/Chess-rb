a19b9683ada15b1f40266830dac220794b1372a8
HereIsSee <biramantvydas@gmail.com>
2024-08-15 00:48:15 +0300

    Add #get_moves in King class
    This still does not account for enemy attacking squares.
    
    This will be implemented in a different method in the king class
    by getting all the enemy attacking moves and then eliminating
    king moves that correspond to the same move as an enemy movie.

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,4 +1,14 @@
-class King
+require_relative 'piece'
+
+class King < Piece
+  
+  KING_MOVES = [
+  [0, 1], [0, -1],
+  [1, 0], [-1, 0],
+  [1, 1], [-1, 1],
+  [1, -1], [-1, -1]
+  ]
+  
   def initialize(color)
     @color = color
   end
@@ -12,5 +22,21 @@ class King
     return 'bk' if @color == 'black'
     
     'error'
+  end
+
+  # add checking for if new move makes the king move into the enemy line of fire
+  def get_moves(current_position, board)
+    moves = []
+
+    KING_MOVES.each do |move|
+      new_x = current_position[0] + move[0]
+      new_y = current_position[1] + move[1]
+      
+      new_position = [new_x, new_y]
+      
+      moves << new_position if is_within_board(new_x, new_y) && !friendly_piece?(board[new_x][new_y])
+    end
+
+    moves
   end
 end
