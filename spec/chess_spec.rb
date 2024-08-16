149701263c8cab0e204425b65487913aaf0f95c2
HereIsSee <biramantvydas@gmail.com>
2024-08-17 01:53:03 +0300

    Delete nonfunctional chess pieces test
    Because the board that is passed to the chess pieces
    is no longer a 2D array containing string representations
    of pieces, but contains the actual pieces the test
    written before no longer work.
    
    They will be partially rewritten in the chess class
    testing. Instead of testing the #get_moves in pieces,
    test will be written for the #generate_legal_moves,
    which will take pieces, their positions and color as
    arguments and return all legal moves available to that
    piece which will effectively replace the previous test.

---
+++ spec/chess_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../lib/chess'
+
+describe Chess do
+
+  describe '#generate_legal_moves' do
+    
+    context 'generate legal moves for pawn' do
+      
+    end
+
+    context 'generate legal moves for rook' do
+      
+    end
+
+    context 'generate legal moves for knight' do
+      
+    end
+
+    context 'generate legal moves for bishop' do
+      
+    end
+
+    context 'generate legal moves for queen' do
+      
+    end
+
+    context 'generate legal moves for king' do
+      
+    end
+    
+  end
+
+end
